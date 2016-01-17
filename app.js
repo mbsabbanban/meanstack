3aaa704f25b083227201522e085caa61f21fa1de
Matt <mbsabbanban@gmail.com>
2016-01-16 22:09:29 -0500

    add: Added Service (FACTORY) and injected into controller

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl',[
+
+app.factory('posts',[function(){
+	var o = {
+		posts: [
+// THIS NO WORK		  {title: 'Kay is HAWT', upvotes: 999};
+		]
+	};
+	return o;
+}]);
+
+app.controller('MainCtrl', [
 '$scope',
-function($scope){
+'posts',
+function($scope, posts){
   $scope.test = 'Hello world!';
   
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-	{title: 'post 2', upvotes: 2},
-	{title: 'post 3', upvotes: 15},
-	{title: 'post 4', upvotes: 9},
-	{title: 'post 5', upvotes: 4}
-  ];
+//  $scope.posts = [
+//    {title: 'post 1', upvotes: 5},
+//	{title: 'post 2', upvotes: 2},
+//	{title: 'post 3', upvotes: 15},
+//	{title: 'post 4', upvotes: 9},
+//	{title: 'post 5', upvotes: 4}
+//  ];
+
+  $scope.posts = posts.posts;
   
   $scope.addPost = function(){
 	  if(!$scope.title || $scope.title === '') {return;} 
