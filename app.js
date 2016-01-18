58579010e23a384d2f519f3ecd8a491492950663
Matt <mbsabbanban@gmail.com>
2016-01-17 22:16:00 -0500

    add: comments and post variable

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ function($stateProvider,$urlRouterProvider){
 		controller: 'MainCtrl'
 	});
 	
+	.state('posts',{
+		url: '/posts/{id}',
+		templateUrl: '/posts.html',
+		controller: 'PostsCtrl'		
+	});
+	
 	$urlRouterProvider.otherwise('home');
 	
 }]);
@@ -44,9 +50,14 @@ function($scope, posts){
   $scope.addPost = function(){
 	  if(!$scope.title || $scope.title === '') {return;} 
 	  $scope.posts.push({
-		  "title": $scope.title,
-		  "link": $scope.link,
-		  "upvotes": 0 });
+		  title: $scope.title,
+		  link: $scope.link,
+		  upvotes: 0,
+		  comments: [
+		    { author: 'Joe', body: 'Cool post!', upvotes: 0},
+			{ author: 'Bob', body: 'Great Idea...not!', upvotes: 0},
+		  ]
+		  });
 	  $scope.title = '';
 	  $scope.link ='';
   };
@@ -55,4 +66,12 @@ function($scope, posts){
 	  post.upvotes += 1;
   };
   
-}])
+}]);
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+	$scope.post = posts.posts[$stateParams.id];
+}]);
