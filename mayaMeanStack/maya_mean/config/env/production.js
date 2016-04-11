a44f38b05bca7141195776556e4849c0bac48857
Matt <mbsabbanban@gmail.com>
2016-04-11 08:19:06 -0400

    add: facebook api credentials for login page

--- mayaMeanStack/maya_mean/config/env/production.js
+++ mayaMeanStack/maya_mean/config/env/production.js
@@ -38,8 +38,8 @@ module.exports = {
     }
   },
   facebook: {
-    clientID: process.env.FACEBOOK_ID || 'APP_ID',
-    clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+    clientID: process.env.FACEBOOK_ID || '1674767872773927', // 'APP_ID',
+    clientSecret: process.env.FACEBOOK_SECRET || '1ec887a574a0c5138db09de60344e112', // 'APP_SECRET',
     callbackURL: '/api/auth/facebook/callback'
   },
   twitter: {
