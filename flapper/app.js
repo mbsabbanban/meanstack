3d9d7b48b7a2b96914571d80d98dd919cb2e6960
Matt Sabban <mbsabbanban@gmail.com>
2016-11-06 19:50:24 -0500

    add: basic html and beginning angular stuff

--- flapper/app.js
+++ flapper/app.js
@@ -1,1 +1,15 @@
-# App.js file for FlapperApp
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', [ '$scope', function($scope){
+  $scope.test = 'Hello World!';
+
+  // Logic that Displays Lists
+  $scope.posts = [
+    'post 1',
+    'post 2',
+    'post 3',
+    'post 4',
+    'post 5'
+  ];
+
+}]);
